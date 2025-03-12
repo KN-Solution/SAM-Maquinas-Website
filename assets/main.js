41569ec32e30bbf5797f1b9de7ff968bab8263bf
Kalil J. Fakhouri <kjfakhouri@gmail.com>
2025-03-12 20:48:01 -0300

    UPDATE
    MAIN BODY
    SECTIONS
    FOOTER

--- assets/main.js
+++ assets/main.js
@@ -11,3 +11,11 @@ function closeMenu(){
         x: '0%'
     })
 }
+
+const homeSwiper = new Swiper("#home-swiper", {
+    effect: "fade",
+    slidesPerView: 1,
+    autoplay: {
+        delay: 3000
+    }
+})
